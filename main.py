5034cb3b4a5a9394d8e35cb1d94b7ca7ae17fa20
CH Lim <chl@chl.com>
2020-04-20 14:39:17 +0800

    use context manager instead of closing opened files manually.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import itertools
 import functools
 import io
+import contextlib
 
 
 OUTPUT_DIR = pathlib.Path('output')
@@ -61,12 +62,14 @@ def main():
     pdf_name = get_filename()
 
     pdf_files = get_pdf_files(INPUT_DIR)
-    pdf_files = map(lambda p: p.open(mode='rb'), pdf_files)
 
-    output_pdf = merge_pdf(pdf_files)
-    save(pdf_name, output_pdf)
-    
-    for file in pdf_files:
-        file.close()
+    with contextlib.ExitStack() as stack:
+        # open each file and put them into the stack.
+        # the stack will close the files automatically 
+        pdf_files = [path.open(mode='rb') for path in pdf_files]
+        pdf_files = map(stack.enter_context, pdf_files)
+
+        output_pdf = merge_pdf(pdf_files)
+        save(pdf_name, output_pdf)
 
 main()
