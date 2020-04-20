9c34657728955316147b71d869d0d6590060b453
CH Lim <chl@chl.com>
2020-04-20 14:12:30 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import PyPDF2 as pdf
+import pathlib
+import sys
+import itertools
+import functools
+import io
+
+
+OUTPUT_DIR = pathlib.Path('output')
+INPUT_DIR = pathlib.Path('input')
+
+
+def get_pdf_files(dir):
+    return list(sorted(list(dir.iterdir())))
+
+def to_pages(file):
+    reader = pdf.PdfFileReader(file)
+    pages = [reader.getPage(page) for page in range(reader.getNumPages())]
+    return pages
+
+def merge_pdf(pdf_files):
+    pages = map(to_pages, pdf_files)
+    pages = itertools.chain.from_iterable(pages)
+
+    writer = pdf.PdfFileWriter()
+    for p in pages:
+        writer.addPage(p)
+
+    return writer
+
+
+def remove_if_already_exists(file):
+    try:
+        file.unlink()
+    except:
+        pass
+
+def save(filename, writer):
+    pdf_file = OUTPUT_DIR / filename
+
+    try:
+        pdf_file = io.FileIO(pdf_file, mode='w')
+    except:
+        pdf_file.unlink()
+        pdf_file = io.FileIO(pdf_file, mode='w')
+
+
+    writer.write(pdf_file)
+
+
+def get_filename():
+    try:
+        filename = sys.argv[1]
+    except:
+        filename = 'untitled.pdf'
+
+    return filename
+
+
+def main():
+    pdf_name = get_filename()
+
+    pdf_files = get_pdf_files(INPUT_DIR)
+    pdf_files = map(lambda p: p.open(mode='rb'), pdf_files)
+
+    output_pdf = merge_pdf(pdf_files)
+    save(pdf_name, output_pdf)
+    
+    for file in pdf_files:
+        file.close()
+
+main()
